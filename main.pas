198f7f97f7dac4d367997184759675f4a02132a7
Ted Szylowiec <tedszy@gmail.com>
2023-11-14 12:24:01 -0500

    Helper functions for dealing with fields, instructions, addresses.

--- main.pas
+++ main.pas
@@ -3,10 +3,11 @@
 program main;
 
 uses
-   mix_machine, mix_show, mix_exec; 
+   mix_machine, mix_show, mix_exec, SysUtils; 
 
 var
    MyInstruction: MIXWord;
+   MyField: Field;
    
 begin
    
@@ -14,23 +15,36 @@ begin
    ShowMIXCharTable;
    InitMix;
 
-   rA[0] := 1;
-   rA[1] := 2;
-   rA[3] := 3;
-   rA[4] := 4;
-   rA[5] := 5;
-
    Poke(0, MakeMIXWord(11,22,33,44,55,13));
 
    ShowMIXState;
    ShowMIXMemory(0,10);
 
-   MyInstruction := MakeMIXWord(0, 0, 0, 0, 0, 8);
+   // Tests.
+
+   MyInstruction := MakeMIXWord(0, 2000 div 64, 2000 mod 64, 2, 3, 8);
    Execute(MyInstruction);
+   
+   writeln;
+
+   MyInstruction := MakeMIXWord(1, 2000 div 64, 2000 mod 64, 4, 5, 8);
+   Execute(MyInstruction);
+
+   writeln;   
 
    MyInstruction := MakeMIXWord(0, 0, 0, 0, 0, 7);
    Execute(MyInstruction);
 
+   MyField.Start := 0;
+   MyField.Stop := 3;
+   writeln('encode (0,3) => ', EncodeField(MyField));
+
+   MyField := DecodeField(11);
+   writeln(format('decode %d => (%d,%d)', [11, MyField.Start, MyField.Stop]));
+
+   MyField.Start := 0;
+   MyField.Stop := 5;
+   writeln('encode (0,5) => ', EncodeField(MyField));
 
    
 end.
--- mix_exec.pas
+++ mix_exec.pas
@@ -1,4 +1,4 @@
-{$mode objfpc}{$R+}{$H+}
+{$mode objfpc}{$R+}{$H+}{$ASSERTIONS ON}
 
 unit mix_exec;
 
@@ -7,18 +7,107 @@ interface
 uses
    mix_machine;
 
+type
+   Field = record
+      start: byte;
+      stop: byte;
+   end;
+
+function EncodeField(F: Field): MIXByte;
+function DecodeField(B: MIXByte): Field;
+
+function MakeInstruction(C_Opcode: MIXByte;
+                         Address: integer;
+                         I_Index: MIXByte;
+                         F_Modifier: MIXByte): MIXWord;
+
 procedure Execute(Instruction: MIXWord);
 
 implementation
 
+{ 
+
+  Instruction layout:
+
+  [  +- A A  |  I  |  F  |  C  ] 
+
+}
+
+function GetAddress(Instruction: MIXWord): integer;
+var
+   Address: integer;
+   Sign: byte;
+begin
+   sign := Instruction[0];
+   Assert((sign = 0) or (sign = 1), 'GetAddress: bad sign byte.');
+   Address := Instruction[1]*MIXByteValues + Instruction[2];
+   if Sign = 0 then
+      GetAddress := Address
+   else
+      GetAddress := -Address;
+end;
+
+function EncodeField(F: Field): MIXByte;
+begin
+   EncodeField := F.Start*8 + F.Stop;
+end;
+
+function DecodeField(B: MIXByte): Field;
+var
+   F: Field;
+begin
+   F.Start := B div 8;
+   F.Stop := B mod 8;
+   DecodeField := F;
+end;
+
+
+// OP ADDRESS,I(F)
+function MakeInstruction(C_Opcode: MIXByte;
+                         Address: integer;
+                         I_Index: MIXByte;
+                         F_Modifier: MIXByte): MIXWord;
+var
+   Sign, A1, A2: byte;
+begin
+   if Address < 0 then Sign := 1 else Sign := 0;
+   A1 := abs(Address) div 64;
+   A2 := abs(Address) mod 64;
+   MakeInstruction := MakeMIXWord(Sign, A1, A2, I_Index, F_Modifier, C_OpCode); 
+end;
+
+
 
 
 procedure Execute(Instruction: MIXWord);
+var
+   C_OpCode: MIXByte; 
+   F_Modifier: MIXByte;
+   I_Index: MIXByte;
+   Address: integer;
+   
 begin
-   case Instruction[5] of
+   C_OpCode := Instruction[5];
+   F_Modifier := Instruction[4];
+   I_Index := Instruction[3];
+   Address := GetAddress(Instruction);
+   case C_OpCode of
 
-      8: writeln('LDA');
 
+      
+      8: begin
+            writeln('LDA');
+            writeln('C:  ', C_OpCode);
+            writeln('F:  ', F_Modifier);
+            writeln('I:  ', I_Index);
+            writeln('+-AA: ', Address);
+            
+            
+         end;
+
+
+
+      
    else
       writeln('Instruction not implemented yet.');      
    end;
